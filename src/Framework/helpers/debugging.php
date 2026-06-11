9797194b636c63a6077182b2755f7f2a6c5c4aac
M-dev-acc <75662164+M-dev-acc@users.noreply.github.com>
2026-06-11 16:50:36 +0530

    Add chore: debugging sugar functions

---
+++ src/Framework/helpers/debugging.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+if (!function_exists('dd')) {
+    function dd(mixed ...$vars) : never {
+        foreach ($vars as $value) {
+            echo "<pre>";
+            var_dump($value);
+            echo "</pre>";
+        }
+        die;
+    }    
+}
+
