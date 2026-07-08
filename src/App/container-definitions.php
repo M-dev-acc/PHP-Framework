6f1bebdb841955a960c13cbaaa1186b945f9daeb
M-dev-acc <75662164+M-dev-acc@users.noreply.github.com>
2026-07-08 20:55:19 +0530

    Add module: database connection

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -1,11 +1,23 @@
 <?php
+
 declare(strict_types=1);
 
 use App\Config\Path;
 use App\Services\ValidatorService;
+use Framework\Database;
 use Framework\TemplateEngine;
 
 return [
     TemplateEngine::class => fn () => new TemplateEngine(Path::VIEW),
     ValidatorService::class => fn () => new ValidatorService(),
+    Database::class => fn () => new Database(
+        'mysql',
+        [
+            'host' => "localhost",
+            'port' => 3306,
+            'dbname' => 'basic_framework',
+        ],
+        'root',
+        ''
+    ),
 ];
